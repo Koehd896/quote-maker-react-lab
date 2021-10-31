252b32f6e3f2b1220072ebe81ae1f33749ad887b
Raven <ravenkoehler@gmail.com>
2021-10-31 11:04:54 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,8 @@
-// TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+    return {
+        type: "ADD_QUOTE",
+        id: quote.id,
+        content: quote.content,
+        author: quote.author
+    }
+}
